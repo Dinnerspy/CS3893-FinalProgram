c409cefa8969a45776b7b6890afc827388eca2f6
Nicho <Nicho@DESKTOP-PC8RCMK>
2019-12-01 02:17:49 -0400

    Added a lot of stuff

--- ChatroomServer/src/chatroomserver/ChatroomServer.java
+++ ChatroomServer/src/chatroomserver/ChatroomServer.java
@@ -7,20 +7,44 @@ package chatroomserver;
 
 import java.net.*;
 import java.io.*;
+import java.util.HashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
+import java.util.Set;
+import java.util.HashSet;
 /**
  *
  * @author Nicholas Ouellette #3556762
  */
 public class ChatroomServer {
 
+    static HashMap<String, String> UserDB = new HashMap<String, String>();
+    static Set<PrintWriter> outputList = new HashSet<>();
     /**
      * @param args the command line arguments
      * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
+
+        final File folder = new File("ChatroomData/Users");
+        //file reading for filling user data base
+        for (final File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+
+            } else {
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(
+                                new FileInputStream(folder + "/" + fileEntry.getName()), "UTF8"));
+                String str;
+
+                str = in.readLine();
+                     
+                
+                 UserDB.put(fileEntry.getName().substring(0, fileEntry.getName().length() -4), str);
+            }
+        }
+
+        //starts up communcation
         try {
             ServerSocket server = new ServerSocket(1998);
             System.out.println("Server started on port " + server.getLocalPort());
@@ -28,8 +52,8 @@ public class ChatroomServer {
             Socket client;
             while (true) {
                 client = server.accept();
-                System.out.println("Client connected from ip address:"+ client.getRemoteSocketAddress().toString().replaceFirst(":\\d*", "") +"port: 1998");
-                ClientPool.execute(new ServerClientRunner(client));
+                System.out.println("Client connected from ip address:" + client.getRemoteSocketAddress().toString().replaceFirst(":\\d*", "") + "port: 1998");
+                ClientPool.execute(new ServerClientRunner(client,UserDB,outputList ));
             }
         } catch (IOException i) {
             System.out.println(i);
--- ChatroomServer/src/chatroomserver/ServerClientRunner.java
+++ ChatroomServer/src/chatroomserver/ServerClientRunner.java
@@ -5,9 +5,13 @@
  */
 package chatroomserver;
 
+import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.io.FileWriter;
+import java.util.Set;
 
 /**
  *
@@ -18,9 +22,14 @@ public class ServerClientRunner implements Runnable {
     Socket ClientSocket;
     Scanner in;
     PrintWriter out;
+    HashMap<String, String> UserDB;
+    String temp, UserName;
+    Set<PrintWriter> outputList;
 
-    public ServerClientRunner(Socket user) {
+    public ServerClientRunner(Socket user, HashMap<String, String> UserDB, Set<PrintWriter> outputList) {
         this.ClientSocket = user;
+        this.UserDB = UserDB;
+        this.outputList = outputList;
     }
 
     @Override
@@ -29,12 +38,100 @@ public class ServerClientRunner implements Runnable {
             in = new Scanner(ClientSocket.getInputStream());
             out = new PrintWriter(ClientSocket.getOutputStream(), true);
             while (true) {
-              
+
+                temp = in.nextLine();
+                System.out.println("Address " + ClientSocket.getRemoteSocketAddress().toString().replaceFirst(":\\d*", "") + temp);
+
+                String[] request = temp.split(":");
+                System.out.println(request[1] + " " + request[2]);
+                //user is attempint to login
+                if (temp.contains("LOGINREQUEST")) {
+
+                    if (userLoginChecker(request[1], request[2])) {
+                        out.println("LOGINREQUEST:ACCEPTED");
+                        UserName = request[1];
+                        break;
+                    } else {
+                        out.println("LOGINREQUEST:REJECTED");
+                    }
+
+                } else if (temp.contains("REGISTERREQUEST")) {
+
+                    if (userRegisterChecker(request[1], request[2])) {
+
+                        out.println("REGISTERREQUEST:ACCEPTED");
+
+                    } else {
+                        out.println("REGISTERREQUEST:REJECTED");
+
+                    }
+                } else {
+
+                    out.println(":Error:");
+                }
+
             }
-        } catch (Exception e) {
+            for (PrintWriter writer : outputList) {
+                        writer.println(UserName + " has joined!");
+                    }
+            out.println(UserName + " has joined!");
+            outputList.add(out);
+            while (true) {
+                String input = in.nextLine();
+                if (input.startsWith("CLIENTLOGOUT:")) {
+                    
+                    for (PrintWriter writer : outputList) {
+                        writer.println(UserName + " has left!");
+                    }
+                    return;
+
+                }
+
+                System.out.println(UserName + ClientSocket.getRemoteSocketAddress().toString().replaceFirst(":\\d*", "") + ": " + input);
+
+                for (PrintWriter writer : outputList) {
+                    writer.println(UserName + ": " + input);
+                }
+            }
+
+        } catch (IOException e) {
             System.out.println(e);
         }
 
+    }
+
+    public synchronized boolean userLoginChecker(String username, String password) {
+
+        if (UserDB.containsKey(username)) {
+
+            if (UserDB.get(username).matches(password)) {
+
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+    public synchronized boolean userRegisterChecker(String username, String password) throws IOException {
+
+        String path = "ChatroomData/Users/";
+
+        if (!UserDB.containsKey(username)) {
+
+            try (FileWriter myWriter = new FileWriter(path + username + ".txt")) {
+                myWriter.write(password);
+                myWriter.close();
+            }
+
+            UserDB.put(username, password);
+
+            return true;
+
+        }
+
+        return false;
     }
 
 }
