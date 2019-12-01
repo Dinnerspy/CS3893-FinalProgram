e2b63639a9f9bf813a8e07126cff8197c1ccce5d
Nicho <Nicho@DESKTOP-PC8RCMK>
2019-12-01 02:09:46 -0400

    Added a lot of stuff

--- ChatroomClient/src/chatroomclient/ChatroomClient.java
+++ ChatroomClient/src/chatroomclient/ChatroomClient.java
@@ -1,30 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package chatroomclient;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.util.Scanner;
-
-/**
- *
- * @author Nicholas Ouellette #3556762
- */
-public class ChatroomClient {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws IOException {
-        Scanner in;
-        PrintWriter out;
-        Socket socket = new Socket("192.168.2.250", 1998);
-        in = new Scanner(socket.getInputStream());
-        out = new PrintWriter(socket.getOutputStream(), true);
-    }
-
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package chatroomclient;
+//
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//import java.net.Socket;
+//import java.util.Scanner;
+//
+///**
+// *
+// * @author Nicholas Ouellette #3556762
+// */
+//public class ChatroomClient {
+//
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) throws IOException {
+//        Scanner in;
+//        PrintWriter out;
+//        Socket socket = new Socket("192.168.2.250", 1998);
+//        in = new Scanner(socket.getInputStream());
+//        out = new PrintWriter(socket.getOutputStream(), true);
+//        
+//    }
+//
+//}
---
+++ ChatroomClient/src/chatroomclient/clientInputRunner.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatroomclient;
+
+import java.util.Scanner;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author Nicho
+ */
+public class clientInputRunner implements Runnable {
+
+    Scanner in;
+    javax.swing.JTextArea jTextArea1;
+
+    public clientInputRunner(Scanner in, JTextArea jTextArea1) {
+        this.in = in;
+        this.jTextArea1 = jTextArea1;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
+
+                jTextArea1.append(line+"\n");
+            }
+        } finally {
+
+            System.out.println("Major error has happened");
+        }
+    }
+
+}
